c513e3084046be7ac3d9ee0ed5242f2537a7c408
spencer-in-github <zhang99@stanford.edu>
2023-05-31 23:32:17 -0700

    p4-initialize

---
+++ P4-test-data.R
@@ -0,0 +1,12 @@
+library(dplyr)
+
+
+# create random data ----
+
+# block group size
+SIZE = 100
+# population data
+pop = sample(x=600:3000, size=SIZE)
+# mileage data
+mileage_per_capita = sample(x=1:100, size=SIZE)
+mileage = pop * mileage_per_capita
